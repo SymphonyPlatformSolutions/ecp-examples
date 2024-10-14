e3f052b790d8332f2548e70617db60a99898e7e8
mistryvinay <vinay@symphony.com>
2024-10-14 14:01:14 +0100

    Added Nav search bar  to NoMatch function.

--- AppExamples/CleverDeal.React/src/Components/ContentDistribution/ContentDistribution.js
+++ AppExamples/CleverDeal.React/src/Components/ContentDistribution/ContentDistribution.js
@@ -213,6 +213,15 @@ function NoMatch() {
             ))}
           </nav>
         </div>
+        <div className="flex items-center space-x-4">
+          <div className="relative">
+            <input type="text" placeholder="Search" className="bg-gray-700 text-white rounded-full py-2 px-4 pl-10 w-40 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-200" />
+            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
+          </div>
+          <button className="bg-blue-600 rounded-full p-2 hover:bg-blue-700 transition-colors duration-200">
+            <Menu className="w-5 h-5" />
+          </button>
+        </div>
       </header>
       <span className="text-sm text-gray-400 flex items-center"></span>
       <p className="text-center text-2xl text-gray-400 mt-6">
