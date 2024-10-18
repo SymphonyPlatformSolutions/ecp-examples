b1335ffdac0b7fc027e7a9cb9175bb95219b1865
mistryvinay <vinay@symphony.com>
2024-10-18 16:06:37 +0100

    Updated tailwind.config.js

--- AppExamples/CleverDeal.React/tailwind.config.js
+++ AppExamples/CleverDeal.React/tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  './src/Components/ContentDistribution/index.js',
-  './src/Components/ContentDistribution/ContentDistribution.js',
-  './src/Components/ContentDistribution/MarketFeed.js',
-  './src/Components/ContentDistribution/MarketFlow.js'
+  './src/Components/ContentDistribution/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
