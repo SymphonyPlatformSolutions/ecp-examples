a189b83d77ad39a0a85bc109ccfbc573d7bfd7ec
Antoine Rollin <antoine.rollin@symphony.com>
2024-10-30 18:49:36 +0100

    Reapply "test"
    This reverts commit 95ea12b1da959f2970404264ccff98b83879ec08.

--- AppExamples/CleverDeal.React/src/Components/ContentDistribution/ContentDistribution.js
+++ AppExamples/CleverDeal.React/src/Components/ContentDistribution/ContentDistribution.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Routes, Route, Link, useLocation } from "react-router-dom";
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-import { Search, Menu, TrendingUp, TrendingDown } from 'lucide-react';
+import { Menu, Search, TrendingDown, TrendingUp } from 'lucide-react';
+import { Link, Route, Routes, useLocation } from "react-router-dom";
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import MarketFeed from './MarketFeed';
 import MarketFlow from './MarketFlow';
 
@@ -42,15 +41,16 @@ const getNavItems = () => [
 
 export const ContentDistribution = () => {
   // dynamic import of tailwind css so that other components are not affected
-  require("./index.css"); 
+  require("./tailwind.css");
+
   return (
     <div className="ContentDistribution">
-       <Routes>
-          <Route index element={<Home />} />
-            <Route path="feed" element={<Feed />} />
-            <Route path="*" element={<NoMatch />} />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="feed" element={<Feed />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
-     </div>
+    </div>
   );
 };
 
